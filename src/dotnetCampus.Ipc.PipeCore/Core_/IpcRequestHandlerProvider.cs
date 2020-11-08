6de88e61787ac185999ded81210eb3cb6d1be1f8
lindexi <lindexi_gd@163.com>
2020-11-08 20:59:40 +0800

    加上关联

---
+++ src/dotnetCampus.Ipc.PipeCore/Core_/IpcRequestHandlerProvider.cs
@@ -0,0 +1,24 @@
+﻿using dotnetCampus.Ipc.Abstractions;
+using dotnetCampus.Ipc.PipeCore.Context;
+using dotnetCampus.Ipc.PipeCore.IpcPipe;
+
+namespace dotnetCampus.Ipc.PipeCore
+{
+    /// <summary>
+    /// 关联 <see cref="ResponseManager"/> 和 <see cref="IIpcRequestHandler"/> 的联系
+    /// </summary>
+    class IpcRequestHandlerProvider
+    {
+        public IpcRequestHandlerProvider(IpcContext ipcContext)
+        {
+            IpcContext = ipcContext;
+        }
+
+        public IpcContext IpcContext { get; }
+
+        public void HandleRequest(PeerProxy sender, IpcClientRequestArgs args)
+        {
+
+        }
+    }
+}
