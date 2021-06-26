76706115dd3dd57da2e01d9edc182fca1fd8747d
lindexi <lindexi_gd@163.com>
2021-06-26 19:16:32 +0800

    加上注释

--- src/dotnetCampus.Ipc.Abstractions/IpcRequestMessage.cs
+++ src/dotnetCampus.Ipc.Abstractions/IpcRequestMessage.cs
@@ -3,16 +3,30 @@ using dotnetCampus.Ipc.PipeCore.Context;
 
 namespace dotnetCampus.Ipc.Abstractions
 {
+    /// <summary>
+    /// 表示一条 IPC 请求消息
+    /// </summary>
     public readonly struct IpcRequestMessage
     {
+        /// <summary>
+        /// 创建 IPC 请求消息
+        /// </summary>
+        /// <param name="summary"></param>
+        /// <param name="requestMessage"></param>
         public IpcRequestMessage(string summary, IpcBufferMessage requestMessage)
         {
             Summary = summary;
             RequestMessage = requestMessage;
         }
 
+        /// <summary>
+        /// 消息的调试友好文本，用来描述这条消息是什么
+        /// </summary>
         public string Summary { get; }
 
+        /// <summary>
+        /// 请求的信息
+        /// </summary>
         public IpcBufferMessage RequestMessage { get; }
     }
 }
