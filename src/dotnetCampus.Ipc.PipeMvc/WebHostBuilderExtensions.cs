f2c4ff06d906c7c36d1cb8136516912d87284d39
lindexi <lindexi_gd@163.com>
2021-12-10 15:55:17 +0800

    重新打包作为库

--- src/dotnetCampus.Ipc.PipeMvc/WebHostBuilderExtensions.cs
+++ src/dotnetCampus.Ipc.PipeMvc/WebHostBuilderExtensions.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -19,11 +19,6 @@ namespace dotnetCampus.Ipc.PipeMvc
     /// </summary>
     public static class WebHostBuilderExtensions
     {
-        static void Main(string[] args)
-        {
-            
-        }
-
         /// <summary>
         /// Enables the <see cref="TestServer" /> service.
         /// </summary>
