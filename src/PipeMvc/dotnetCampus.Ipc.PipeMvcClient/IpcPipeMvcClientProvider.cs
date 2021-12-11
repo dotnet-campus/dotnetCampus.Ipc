e5ce4429be4cd4836286bbbfb241e7e7fe5f9946
lindexi <lindexi_gd@163.com>
2021-12-11 12:05:13 +0800

    修改命名

--- src/PipeMvc/dotnetCampus.Ipc.PipeMvcClient/IpcPipeMvcClientProvider.cs
+++ src/PipeMvc/dotnetCampus.Ipc.PipeMvcClient/IpcPipeMvcClientProvider.cs
@@ -11,7 +11,7 @@ namespace dotnetCampus.Ipc.PipeMvcClient
     /// <summary>
     /// 提供给客户端调用 MVC 的 Ipc 服务的功能
     /// </summary>
-    public static class PipeMvcClientProvider
+    public static class IpcPipeMvcClientProvider
     {
         /// <summary>
         /// 获取访问 Mvc 的 Ipc 服务的对象
@@ -19,7 +19,7 @@ namespace dotnetCampus.Ipc.PipeMvcClient
         /// <param name="ipcPipeMvcServerName">对方 Ipc 服务名</param>
         /// <param name="clientIpcProvider">可选，用来进行 Ipc 连接的本地服务。如不传或是空，将创建新的 Ipc 连接服务</param>
         /// <returns></returns>
-        public static async Task<HttpClient> CreateIpcMvcClient(string ipcPipeMvcServerName,IpcProvider? clientIpcProvider = null)
+        public static async Task<HttpClient> CreateIpcMvcClient(string ipcPipeMvcServerName, IpcProvider? clientIpcProvider = null)
         {
             if (clientIpcProvider == null)
             {
