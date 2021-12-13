b6544ae3051808ff61ad81be1bc4af424f01825b
walterlv <xblero@gmail.com>
2021-12-13 14:41:32 +0800

    添加生成对象传递的单元测试

---
+++ tests/dotnetCampus.Ipc.Tests/CompilerServices/Fake/FakeIpcObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using dotnetCampus.Ipc.CompilerServices.Attributes;
+
+namespace dotnetCampus.Ipc.Tests.CompilerServices
+{
+    [IpcPublic(typeof(IFakeIpcObject), typeof(FakeIpcObjectIpcProxy), typeof(FakeIpcObjectIpcJoint))]
+    internal class FakeIpcObject : IFakeIpcObject
+    {
+        public async Task<(double a, uint b, int c, byte d)> ConstructAsync(double a, uint b, int c, byte d)
+        {
+            return (a, b, c, d);
+        }
+
+        public async Task<FakeIpcObjectSubModelA> ConvertObjectAsync(FakeIpcObjectSubModelA model)
+        {
+            return new FakeIpcObjectSubModelA(model.A, model.B, model.C, model.D);
+        }
+
+        public async Task<string> ConvertStringAsync(string source)
+        {
+            return source;
+        }
+    }
+}
---
+++ tests/dotnetCampus.Ipc.Tests/CompilerServices/Fake/FakeIpcObjectIpcJoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using dotnetCampus.Ipc.CompilerServices.GeneratedProxies;
+
+namespace dotnetCampus.Ipc.Tests.CompilerServices
+{
+    internal class FakeIpcObjectIpcJoint : GeneratedIpcJoint<IFakeIpcObject>
+    {
+        protected override void MatchMembers(IFakeIpcObject real)
+        {
+            MatchMethod(nameof(IFakeIpcObject.ConstructAsync), new Func<double, uint, int, byte, Task<(double a, uint b, int c, byte d)>>((a, b, c, d) => real.ConstructAsync(a, b, c, d)));
+            MatchMethod(nameof(IFakeIpcObject.ConvertObjectAsync), new Func<FakeIpcObjectSubModelA, Task<FakeIpcObjectSubModelA>>((a) => real.ConvertObjectAsync(a)));
+            MatchMethod(nameof(IFakeIpcObject.ConvertStringAsync), new Func<string, Task<string>>((a) => real.ConvertStringAsync(a)));
+        }
+    }
+}
