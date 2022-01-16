9c88f231c18bdfd539dccca8d2037decaf060c6c
lindexi <lindexi_gd@163.com>
2022-01-16 10:30:23 +0800

    加上拷贝信息

--- src/PipeMvc/dotnetCampus.Ipc.PipeMvcServer/HostFramework/RequestLifetimeFeature.cs
+++ src/PipeMvc/dotnetCampus.Ipc.PipeMvcServer/HostFramework/RequestLifetimeFeature.cs
@@ -1,5 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+// Copy From: https://github.com/dotnet/aspnetcore
 
 using System;
 using System.Threading;
