8a8f8955c6829cbc59eba233d6267809863ed8ac
lindexi <lindexi_gd@163.com>
2022-01-21 08:52:28 +0800

    测试多线程

--- demo/PipeMvc/PipeMvcClientDemo/MainWindow.xaml.cs
+++ demo/PipeMvc/PipeMvcClientDemo/MainWindow.xaml.cs
@@ -1,10 +1,10 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 using System.Windows;
-
 using dotnetCampus.Ipc.PipeMvcClient;
-
 using PipeMvcServerDemo;
 
 namespace PipeMvcClientDemo;
@@ -92,15 +92,69 @@ public partial class MainWindow : Window
 
         Log($"[Request][Post] IpcPipeMvcServer://api/Foo");
 
-        var json = JsonSerializer.Serialize(new FooContent
-        {
-            Foo1 = "Foo PostFooWithArgumentButton",
-            Foo2 = null,
-        });
+        var json = JsonSerializer.Serialize(new FooContent { Foo1 = "Foo PostFooWithArgumentButton", Foo2 = null, });
         StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
 
         var response = await _ipcPipeMvcClient.PostAsync("api/Foo/PostFoo", content);
         var m = await response.Content.ReadAsStringAsync();
         Log($"[Response][Post] IpcPipeMvcServer://api/Foo/PostFoo {response.StatusCode} {m}");
+    }
+
+    private void MultiThreadButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (_ipcPipeMvcClient is null)
+        {
+            return;
+        }
+
+        var count = 10;
+        for (int i = 0; i < count; i++)
+        {
+            var id = i;
+            Task.Run(async () =>
+            {
+                Log($"[{id}][Request][Post] IpcPipeMvcServer://api/Foo");
+                var response = await _ipcPipeMvcClient.PostAsync("api/Foo", new StringContent(""));
+                var m = await response.Content.ReadAsStringAsync();
+                Log($"[{id}][Response][Post] IpcPipeMvcServer://api/Foo {response.StatusCode} {m}");
+
+                var a = Random.Shared.Next();
+                var b = Random.Shared.Next();
+
+                Log($"[{id}][Request][Get] IpcPipeMvcServer://api/Foo/Add?a={a}&b={b}");
+                m = await _ipcPipeMvcClient.GetStringAsync($"api/Foo/Add?a={a}&b={b}");
+                Log($"[{id}][Response][Get] IpcPipeMvcServer://api/Foo/Add {m} {a}+{b}={a + b}");
+
+                Log($"[{id}][Request][Post] IpcPipeMvcServer://api/Foo");
+                response = await _ipcPipeMvcClient.PostAsync("api/Foo", new StringContent(BuildRandomText()));
+                m = await response.Content.ReadAsStringAsync();
+                Log($"[Response][Post] IpcPipeMvcServer://api/Foo {response.StatusCode} {m}");
+
+                Log($"[{id}][Request][Post] IpcPipeMvcServer://api/Foo");
+
+                var json = JsonSerializer.Serialize(new FooContent
+                {
+                    Foo1 = Random.Shared.Next(2) == 1 ? BuildRandomText() : null,
+                    Foo2 = Random.Shared.Next(2) == 1 ? BuildRandomText() : null,
+                });
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                response = await _ipcPipeMvcClient.PostAsync("api/Foo/PostFoo", content);
+                m = await response.Content.ReadAsStringAsync();
+                Log($"[{id}][Response][Post] IpcPipeMvcServer://api/Foo/PostFoo {response.StatusCode} {m}");
+            });
+        }
+    }
+
+    private static string BuildRandomText()
+    {
+        var count = 10;
+        var text = new StringBuilder();
+        for (int i = 0; i < count; i++)
+        {
+            text.Append((char) Random.Shared.Next('a', 'z' + 1));
+        }
+
+        return text.ToString();
     }
 }
