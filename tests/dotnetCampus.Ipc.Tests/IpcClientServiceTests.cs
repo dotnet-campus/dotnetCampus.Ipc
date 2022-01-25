9969580d5958444bacca7684e1ff4dfdda48cebf
lindexi <lindexi_gd@163.com>
2022-01-25 09:20:54 +0800

    修复单元测试

--- tests/dotnetCampus.Ipc.Tests/IpcClientServiceTests.cs
+++ tests/dotnetCampus.Ipc.Tests/IpcClientServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using dotnetCampus.Ipc.Messages;
@@ -49,10 +50,23 @@ namespace dotnetCampus.Ipc.Tests
                     ipcProviderB.Dispose();
                 });
 
-                await Assert.ThrowsExceptionAsync<ObjectDisposedException>(async () =>
+                try
                 {
+                    // 等待发送和释放任务完成
+                    // 预期是一定会在发送任务抛出异常，异常是 ObjectDisposedException 或 IOException 这两个
                     await Task.WhenAll(sendTask, disposeTask);
-                });
+                }
+                catch (Exception e)
+                {
+                    // 期望抛出异常
+                    // 在调用一次 WriteMessageAsync 之前，被释放，那么抛出 ObjectDisposedException 异常
+                    // 在写入过程，被释放，那么写入 PipeStream.WriteCore 抛出 IO 异常
+                    Assert.AreEqual(true, e is ObjectDisposedException or IOException);
+                    return;
+                }
+
+                // 如果没有抛出异常，那么进入此分支
+                Assert.Fail("预期抛出的是 ObjectDisposedException 或 IOException 异常");
             });
         }
     }
