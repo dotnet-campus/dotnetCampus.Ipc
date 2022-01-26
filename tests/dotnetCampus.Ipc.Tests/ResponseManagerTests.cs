efc253ad34560a5ee7afed6b62f973df7518a393
lindexi <lindexi_gd@163.com>
2022-01-26 15:58:32 +0800

    修复单元测试

--- tests/dotnetCampus.Ipc.Tests/ResponseManagerTests.cs
+++ tests/dotnetCampus.Ipc.Tests/ResponseManagerTests.cs
@@ -57,7 +57,7 @@ namespace dotnetCampus.Ipc.Tests
         [ContractTestCase]
         public void GetResponseAsync()
         {
-            "发送消息之后，能等待收到对应的回复".Test(() =>
+            "发送消息之后，能等待收到对应的回复".Test(async () =>
             {
                 var ipcMessageRequestManager = new IpcMessageRequestManager(new IpcProvider().IpcContext);
                 var requestByteList = new byte[] { 0xFF, 0xFE };
@@ -87,6 +87,8 @@ namespace dotnetCampus.Ipc.Tests
                 ipcMessageRequestManager.OnReceiveMessage(new PeerStreamMessageArgs(new IpcMessageContext(), "Foo", responseStream, ack: 100,
                     IpcMessageCommandType.ResponseMessage));
 
+                // 在 OnReceiveMessage 收到消息，不是立刻释放 ipcClientRequestMessage 的，需要调度到线程池进行释放
+                await ipcClientRequestMessage.Task.WaitTimeout(TimeSpan.FromSeconds(5));
                 Assert.AreEqual(true, ipcClientRequestMessage.Task.IsCompleted);
             });
         }
@@ -94,7 +96,7 @@ namespace dotnetCampus.Ipc.Tests
         [ContractTestCase]
         public void WaitingResponseCount()
         {
-            "所有发送消息都收到回复后，将清空等待响应的数量".Test(() =>
+            "所有发送消息都收到回复后，将清空等待响应的数量".Test(async () =>
             {
                 // 请求的顺序是
                 // A: 生成请求消息
@@ -165,6 +167,9 @@ namespace dotnetCampus.Ipc.Tests
 
                 foreach (var ipcClientRequestMessage in ipcClientRequestMessageList)
                 {
+                    // 在 OnReceiveMessage 收到消息，不是立刻释放 ipcClientRequestMessage 的，需要调度到线程池进行释放
+                    await ipcClientRequestMessage.Task.WaitTimeout();
+
                     Assert.AreEqual(true, ipcClientRequestMessage.Task.IsCompleted);
                 }
             });
