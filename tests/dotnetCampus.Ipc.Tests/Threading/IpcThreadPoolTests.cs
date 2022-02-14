85ba4c9d393e99b470e83ea56480d708a2509455
walterlv <xblero@gmail.com>
2022-02-14 15:15:19 +0800

    线程池满，等待新任务调度时，本递归调用不应出现 StackOverflowException。

---
+++ tests/dotnetCampus.Ipc.Tests/Threading/IpcThreadPoolTests.cs
@@ -0,0 +1,62 @@
+﻿using dotnetCampus.Ipc.Tests.CompilerServices;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using MSTest.Extensions.Contracts;
+using dotnetCampus.Ipc.Pipes;
+using dotnetCampus.Ipc.Messages;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dotnetCampus.Ipc.Context;
+using System.Threading;
+using dotnetCampus.Ipc.Internals;
+
+namespace dotnetCampus.Ipc.Tests.Threading;
+
+[TestClass]
+public class IpcThreadPoolTests : IIpcRequestHandler
+{
+    [ContractTestCase]
+    public void RunRecursively()
+    {
+        "线程池满，等待新任务调度时，本递归调用不应出现 StackOverflowException。".Test(async () =>
+        {
+            var aName = $"IpcObjectTests.IpcTests.IpcThreadPoolTests.{nameof(RunRecursively)}.A";
+            var bName = $"IpcObjectTests.IpcTests.IpcThreadPoolTests.{nameof(RunRecursively)}.B";
+            var aProvider = new IpcProvider(aName, new IpcConfiguration
+            {
+                DefaultIpcRequestHandler = this,
+            });
+            var bProvider = new IpcProvider(bName);
+            aProvider.StartServer();
+            bProvider.StartServer();
+            // 这里的延迟是为了暂时缓解死锁 bug @lindexi
+            await Task.Delay(100);
+            var aPeer = await bProvider.GetAndConnectToPeerAsync(aName);
+
+            // 占满线程池，以便有机会等待新任务调度。
+            var task1 = aPeer.GetResponseAsync(new IpcMessage("Test", Encoding.UTF8.GetBytes("Test")));
+            var task2 = aPeer.GetResponseAsync(new IpcMessage("Test", Encoding.UTF8.GetBytes("Test")));
+            var task3 = aPeer.GetResponseAsync(new IpcMessage("Test", Encoding.UTF8.GetBytes("Test")));
+            var task4 = aPeer.GetResponseAsync(new IpcMessage("Test", Encoding.UTF8.GetBytes("Test")));
+            var task5 = aPeer.GetResponseAsync(new IpcMessage("Test", Encoding.UTF8.GetBytes("Test")));
+            var task6 = aPeer.GetResponseAsync(new IpcMessage("Test", Encoding.UTF8.GetBytes("Test")));
+            var task7 = aPeer.GetResponseAsync(new IpcMessage("Test", Encoding.UTF8.GetBytes("Test")));
+
+            await Task.WhenAll(task1, task2, task3, task4, task5);
+        });
+    }
+
+    public Task<IIpcResponseMessage> HandleRequest(IIpcRequestContext requestContext)
+    {
+        Thread.Sleep(100);
+        return Task.FromResult<IIpcResponseMessage>(
+            new IpcHandleRequestMessageResult(
+                new IpcMessage(
+                    "TestR",
+                    Encoding.UTF8.GetBytes("TestR")
+                )
+            )
+        );
+    }
+}
