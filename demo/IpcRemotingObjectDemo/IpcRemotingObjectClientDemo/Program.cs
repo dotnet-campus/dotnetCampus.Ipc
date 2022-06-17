097f97f293cc809ce3224de501b308627883ec05
lindexi <lindexi_gd@163.com>
2022-06-17 21:00:28 +0800

    删掉注释

--- demo/IpcRemotingObjectDemo/IpcRemotingObjectClientDemo/Program.cs
+++ demo/IpcRemotingObjectDemo/IpcRemotingObjectClientDemo/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using dotnetCampus.Ipc.CompilerServices.GeneratedProxies;
+﻿using dotnetCampus.Ipc.CompilerServices.GeneratedProxies;
 using dotnetCampus.Ipc.Pipes;
 
 using IpcRemotingObjectServerDemo;
