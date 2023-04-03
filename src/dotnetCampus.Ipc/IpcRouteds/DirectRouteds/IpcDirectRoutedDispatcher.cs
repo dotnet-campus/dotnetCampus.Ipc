e17d9ede76fe6de00cca9f3a1944b1c02b6bc4ff
lindexi <lindexi_gd@163.com>
2023-04-03 10:12:18 +0800

    完成最小可使用逻辑

--- src/dotnetCampus.Ipc/IpcRouteds/DirectRouteds/IpcDirectRoutedDispatcher.cs
+++ src/dotnetCampus.Ipc/IpcRouteds/DirectRouteds/IpcDirectRoutedDispatcher.cs
@@ -1,5 +1,6 @@
 ﻿#if NET6_0_OR_GREATER
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using dotnetCampus.Ipc.Context;
+using dotnetCampus.Ipc.Internals;
 using dotnetCampus.Ipc.Messages;
 using dotnetCampus.Ipc.Pipes;
 using dotnetCampus.Ipc.Utils.Extensions;
@@ -36,7 +38,7 @@ public class JsonIpcDirectRoutedProvider
 {
     public JsonIpcDirectRoutedProvider(string? pipeName = null, IpcConfiguration? ipcConfiguration = null)
     {
-        pipeName ??= $"JsonIpcDirectRouted_{Guid.NewGuid().ToString("N")}";
+        pipeName ??= $"JsonIpcDirectRouted_{Guid.NewGuid():N}";
         var ipcProvider = new IpcProvider(pipeName, ipcConfiguration);
         IpcProvider = ipcProvider;
     }
@@ -47,12 +49,64 @@ public class JsonIpcDirectRoutedProvider
     }
 
     public void StartServer()
-    {        
+    {
         IpcProvider.StartServer();
         IpcProvider.IpcServerService.MessageReceived += IpcServerService_MessageReceived;
         var requestHandler = new RequestHandler(this);
         IpcProvider.IpcContext.IpcConfiguration.AddFrameworkRequestHandlers(requestHandler);
     }
+
+    /// <summary>
+    /// 获取对 <see cref="serverPeerName"/> 请求的客户端
+    /// </summary>
+    /// <param name="serverPeerName"></param>
+    /// <returns></returns>
+    public async Task<JsonIpcDirectRoutedClientProxy> GetAndConnectClientAsync(string serverPeerName)
+    {
+        var peer = await IpcProvider.GetAndConnectToPeerAsync(serverPeerName);
+        return new JsonIpcDirectRoutedClientProxy(peer);
+    }
+
+    #region Notify
+
+    public void AddNotifyHandler<T>(string routedPath, Func<T, Task> handler)
+    {
+        // ReSharper disable once AsyncVoidLambda
+        // ReSharper disable once ConvertToLocalFunction
+        Action<T> notifyHandler = async (argument) =>
+        {
+            try
+            {
+                await handler(argument);
+            }
+            catch (Exception e)
+            {
+                // 后台顶层，抛出就没了哦
+                IpcProvider.IpcContext.Logger.Warning($"Handle {routedPath} with exception. {e}");
+            }
+        };
+        AddNotifyHandler(routedPath, notifyHandler);
+    }
+
+    public void AddNotifyHandler<T>(string routedPath, Action<T> handler)
+    {
+        ThrowIfStarted();
+
+        if (!HandleNotifyDictionary.TryAdd(routedPath, NotifyHandler))
+        {
+            throw new InvalidOperationException($"重复添加对 {routedPath} 的处理");
+        }
+
+        void NotifyHandler(MemoryStream stream)
+        {
+            var argument = ToObject<T>(stream);
+            handler(argument!);
+        }
+    }
+
+    private delegate void HandleNotify(MemoryStream stream);
+
+    private ConcurrentDictionary<string, HandleNotify> HandleNotifyDictionary { get; } = new ConcurrentDictionary<string, HandleNotify>();
 
     private void IpcServerService_MessageReceived(object? sender, PeerMessageArgs e)
     {
@@ -65,11 +119,97 @@ public class JsonIpcDirectRoutedProvider
         if (TryHandleMessage(e.Message, out var stream, out var routedPath))
         {
             // 接下来进行调度
-
+            if (HandleNotifyDictionary.TryGetValue(routedPath, out var handleNotify))
+            {
+                handleNotify(stream);
+            }
+            else
+            {
+                IpcProvider.IpcContext.Logger.Warning($"找不到对 {routedPath} 的 {nameof(JsonIpcDirectRoutedProvider)} 处理，是否忘记调用 {nameof(AddNotifyHandler)} 添加处理");
+            }
 
             e.SetHandle("JsonIpcDirectRouted Handled in MessageReceived");
         }
     }
+
+    #endregion
+
+    #region Request Response
+
+    public void AddRequestHandler<TRequest, TResponse>(string routedPath, Func<TRequest, TResponse> handler)
+    {
+        AddRequestHandler<TRequest, TResponse>(routedPath, request =>
+        {
+            var response = handler(request);
+            return Task.FromResult(response);
+        });
+    }
+
+    public void AddRequestHandler<TRequest, TResponse>(string routedPath, Func<TRequest, Task<TResponse>> handler)
+    {
+        ThrowIfStarted();
+        HandleRequest handleRequest = HandleRequest;
+
+        if (!HandleRequestDictionary.TryAdd(routedPath, handleRequest))
+        {
+            throw new InvalidOperationException($"重复添加对 {routedPath} 的处理");
+        }
+
+        async ValueTask<IpcMessage> HandleRequest(MemoryStream stream)
+        {
+            var argument = ToObject<TRequest>(stream);
+            var response = await handler(argument!);
+            var responseMemoryStream = new MemoryStream();
+            using (TextWriter textWriter = new StreamWriter(responseMemoryStream, Encoding.UTF8, leaveOpen: true))
+            {
+                JsonSerializer.Serialize(textWriter, response);
+            }
+
+            var buffer = responseMemoryStream.GetBuffer();
+            var length = (int) responseMemoryStream.Position;
+            return new IpcMessage($"Handle {routedPath} response", new IpcMessageBody(buffer, 0, length));
+        }
+    }
+
+    private delegate ValueTask<IpcMessage> HandleRequest(MemoryStream stream);
+
+    private ConcurrentDictionary<string, HandleRequest> HandleRequestDictionary { get; } =
+        new ConcurrentDictionary<string, HandleRequest>();
+
+    class RequestHandler : IIpcRequestHandler
+    {
+        public RequestHandler(JsonIpcDirectRoutedProvider jsonIpcDirectRoutedProvider)
+        {
+            JsonIpcDirectRoutedProvider = jsonIpcDirectRoutedProvider;
+        }
+        private JsonIpcDirectRoutedProvider JsonIpcDirectRoutedProvider { get; }
+
+        async Task<IIpcResponseMessage> IIpcRequestHandler.HandleRequest(IIpcRequestContext requestContext)
+        {
+            if (JsonIpcDirectRoutedProvider.TryHandleMessage(requestContext.IpcBufferMessage, out var stream, out var routedPath))
+            {
+                using (stream)
+                {
+                    if (JsonIpcDirectRoutedProvider.HandleRequestDictionary.TryGetValue(routedPath, out var handler))
+                    {
+                        var ipcMessage = await handler(stream);
+
+                        IIpcResponseMessage response = new IpcHandleRequestMessageResult(ipcMessage);
+                        return response;
+                    }
+                    else
+                    {
+                        JsonIpcDirectRoutedProvider.IpcProvider.IpcContext.Logger.Warning($"找不到对 {routedPath} 的 {nameof(JsonIpcDirectRoutedProvider)} 处理，是否忘记调用 {nameof(AddRequestHandler)} 添加处理");
+                        return KnownIpcResponseMessages.CannotHandle;
+                    }
+                };
+            }
+
+            return KnownIpcResponseMessages.CannotHandle;
+        }
+    }
+
+    #endregion
 
     private static bool TryHandleMessage(in IpcMessage ipcMessage, [NotNullWhen(true)] out MemoryStream? stream, [NotNullWhen(true)] out string? routedPath)
     {
@@ -90,21 +230,6 @@ public class JsonIpcDirectRoutedProvider
     private JsonSerializer JsonSerializer => _jsonSerializer ??= JsonSerializer.CreateDefault();
     private JsonSerializer? _jsonSerializer;
 
-    public void AddNotifyHandler<T>(string routedPath, Action<T> handler)
-    {
-        ThrowIfStarted();
-
-        Action<MemoryStream> notifyHandler = (MemoryStream stream) =>
-        {
-            using StreamReader reader = new StreamReader(stream, leaveOpen: true);
-            JsonReader jsonReader = new JsonTextReader(reader);
-            var argument = JsonSerializer.Deserialize<T>(jsonReader);
-            handler(argument);
-        };
-
-
-    }
-
     private void ThrowIfStarted()
     {
         if (IpcProvider.IsStarted)
@@ -113,28 +238,11 @@ public class JsonIpcDirectRoutedProvider
         }
     }
 
-    delegate void HandleNotify(MemoryStream stream);
-
-    class RequestHandler : IIpcRequestHandler
+    private T? ToObject<T>(MemoryStream stream)
     {
-        public RequestHandler(JsonIpcDirectRoutedProvider jsonIpcDirectRoutedProvider)
-        {
-            JsonIpcDirectRoutedProvider = jsonIpcDirectRoutedProvider;
-        }
-        private JsonIpcDirectRoutedProvider JsonIpcDirectRoutedProvider { get; }
-
-        public async Task<IIpcResponseMessage> HandleRequest(IIpcRequestContext requestContext)
-        {
-            if (JsonIpcDirectRoutedProvider.TryHandleMessage(requestContext.IpcBufferMessage, out var stream, out var routedPath))
-            {
-                using (stream)
-                {
-
-                };
-            }
-
-            return KnownIpcResponseMessages.CannotHandle;
-        }
+        using StreamReader reader = new StreamReader(stream, leaveOpen: true);
+        JsonReader jsonReader = new JsonTextReader(reader);
+        return JsonSerializer.Deserialize<T>(jsonReader);
     }
 }
 
@@ -184,10 +292,5 @@ public class JsonIpcDirectRoutedClientProxy
 
         return new IpcMessage($"Notify {routedPath}", new IpcMessageBody(buffer, start: 0, length));
     }
-}
-
-internal class IpcDirectRoutedDispatcher
-{
-
 }
 #endif
