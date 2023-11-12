c436e078b8fecaf219ddd3c72dcc2a3ad5e486b3
lindexi <lindexi_gd@163.com>
2023-11-12 17:17:19 +0800

    继续回滚到可以执行的代码

--- demo/dotnetCampus.Ipc.Demo/Program.cs
+++ demo/dotnetCampus.Ipc.Demo/Program.cs
@@ -31,12 +31,9 @@ namespace dotnetCampus.Ipc.Demo
                 // 这是被启动的进程，主动连接发送消息
                 Console.WriteLine($"[{Environment.ProcessId}] 开始连接对方进程");
 
-                var peer = await Task.Run(async () =>
-                {
-                    return await ipcProvider.GetAndConnectToPeerAsync(args[0]);
-                });
                 Task.Run(async () =>
                 {
+                    var peer = await ipcProvider.GetAndConnectToPeerAsync(args[0]);
                     peer.MessageReceived += (sender, messageArgs) =>
                     {
                         Console.WriteLine(
