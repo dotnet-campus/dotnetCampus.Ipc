8c39badfa7fe1ba9a34b64fad0c83ea2f7040381
lindexi <lindexi_gd@163.com>
2023-11-16 17:17:01 +0800

    添加库定义通讯

---
+++ demo/UnoDemo/IpcUno/IpcUno/Business/Models/ConnectedPeerModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+
+using dotnetCampus.Ipc.Context;
+using dotnetCampus.Ipc.Messages;
+using dotnetCampus.Ipc.Pipes;
+
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace IpcUno.Business.Models
+{
+    public class ConnectedPeerModel
+    {
+        public ConnectedPeerModel()
+        {
+            Peer = null!;
+        }
+
+        public ConnectedPeerModel(PeerProxy peer)
+        {
+            Peer = peer;
+            peer.MessageReceived += Peer_MessageReceived;
+        }
+
+        private void Peer_MessageReceived(object? sender, IPeerMessageArgs e)
+        {
+            var streamReader = new StreamReader(e.Message.Body.ToMemoryStream());
+            var message = streamReader.ReadToEnd();
+
+            _ = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => 
+            {
+                AddMessage(PeerName, message);
+            });
+        }
+
+        public void AddMessage(string name, string message)
+        {
+            MessageList.Add($"{name} {DateTime.Now:yyyy/MM/dd hh:mm:ss.fff}:\r\n{message}");
+        }
+
+        public ObservableCollection<string> MessageList { get; } = new ObservableCollection<string>();
+
+        public PeerProxy Peer { get; }
+
+        public string PeerName => Peer.PeerName;
+    }
+}
