797bb6b1f71d35afaf087a8f27e36a26376999df
lindexi <lindexi_gd@163.com>
2023-11-24 16:21:59 +0800

    完成基础的通讯功能

--- demo/UnoDemo/IpcUno/IpcUno/Presentation/ChatPage.xaml.cs
+++ demo/UnoDemo/IpcUno/IpcUno/Presentation/ChatPage.xaml.cs
@@ -1,16 +1,31 @@
+using System.Reflection;
+using System.Text;
+
 namespace IpcUno.Presentation
 {
     public sealed partial class ChatPage : Page
     {
         public ChatPage(ConnectedPeerModel model, string serverName)
         {
+            DataContextChanged += ChatPage_DataContextChanged;
+            DataContext = model;
             this.InitializeComponent();
             Model = model;
             ServerName = serverName;
         }
 
+        private void ChatPage_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+        }
+
         public string ServerName { get; }
 
         public ConnectedPeerModel Model { get; }
+
+        private async void SendButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Model.AddMessage(ServerName, MessageTextBox.Text);
+            await Model.Peer.NotifyAsync(new dotnetCampus.Ipc.Messages.IpcMessage("CharPage", Encoding.UTF8.GetBytes(MessageTextBox.Text)));
+        }
     }
 }
