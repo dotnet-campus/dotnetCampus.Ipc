3da6bd030ac027f0e3cb4cd207e77957dccdd6c3
lindexi <lindexi_gd@163.com>
2024-03-07 09:51:12 +0800

    修改 IPC 的日志模块，让其在业务端有更加正确的输出

--- src/dotnetCampus.Ipc/Utils/Extensions/LoggerExtensions.cs
+++ src/dotnetCampus.Ipc/Utils/Extensions/LoggerExtensions.cs
@@ -8,17 +8,17 @@ namespace dotnetCampus.Ipc.Utils.Extensions
     {
         public static void Trace(this ILogger? logger, string message)
         {
-            logger?.Log(LogLevel.Trace, default, 0, null, (s, e) => message);
+            logger?.Log(LogLevel.Trace, default, message, null, FormatOnlyMessage);
         }
 
         public static void Debug(this ILogger? logger, string message)
         {
-            logger?.Log(LogLevel.Debug, default, 0, null, (s, e) => message);
+            logger?.Log(LogLevel.Debug, default, message, null, FormatOnlyMessage);
         }
 
         public static void Information(this ILogger? logger, string message)
         {
-            logger?.Log(LogLevel.Information, default, 0, null, (s, e) => message);
+            logger?.Log(LogLevel.Information, default, message, null, FormatOnlyMessage);
         }
 
         public static void Warning(this ILogger? logger, string message)
@@ -29,7 +29,7 @@ namespace dotnetCampus.Ipc.Utils.Extensions
             }
             else
             {
-                logger?.Log(LogLevel.Warning, default, 0, null, (s, e) => message);
+                logger?.Log(LogLevel.Warning, default, message, null, FormatOnlyMessage);
             }
         }
 
@@ -41,13 +41,24 @@ namespace dotnetCampus.Ipc.Utils.Extensions
             }
             else
             {
-                logger?.Log(LogLevel.Error, default, 0, null, (s, e) => message);
+                logger?.Log(LogLevel.Error, default, message, null, FormatOnlyMessage);
             }
         }
 
         public static void Error(this ILogger? logger, Exception exception, string? message = null)
         {
-            logger?.Log(LogLevel.Error, default, 0, exception, (s, e) => message ?? "");
+            logger?.Log(LogLevel.Error, default, message, exception, StandardFormatMessage);
         }
+
+        private static Func<string, Exception?, string> FormatOnlyMessage => static (s, _) => s;
+
+        private static Func<string?, Exception?, string> StandardFormatMessage => static (s, e) =>
+        {
+            if (s is null && e != null)
+            {
+                return e.ToString();
+            }
+            return $"[IPC Error] {s} {e}";
+        };
     }
 }
