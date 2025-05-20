b53301881905c1cc77c1d81a5af5b344422614b0
lindexi <lindexi_gd@163.com>
2025-05-20 20:55:59 +0800

    减少数组的分配，直接获取原始的数组信息

--- src/dotnetCampus.Ipc/Utils/IO/ByteListMessageStream.cs
+++ src/dotnetCampus.Ipc/Utils/IO/ByteListMessageStream.cs
@@ -8,9 +8,9 @@ namespace dotnetCampus.Ipc.Utils.IO
     /// <summary>
     /// 数组列表的消息，特别定义类型，方便内存分析
     /// </summary>
-    internal class ByteListMessageStream : MemoryStream
+    internal sealed class ByteListMessageStream : MemoryStream
     {
-        public ByteListMessageStream(byte[] buffer, int count) : base(buffer, 0,
+        private ByteListMessageStream(byte[] buffer, int count) : base(buffer, 0,
             count, writable: false,
             // 设置 publiclyVisible 属性，防止 GetBuffer 抛出 UnauthorizedAccessException 异常
             publiclyVisible: true)
@@ -20,6 +20,9 @@ namespace dotnetCampus.Ipc.Utils.IO
         public ByteListMessageStream(in IpcMessageContext ipcMessageContext) : this(ipcMessageContext.MessageBuffer,
             (int) ipcMessageContext.MessageLength)
         {
+            IpcMessageContext = ipcMessageContext;
         }
+
+        public IpcMessageContext IpcMessageContext { get; }
     }
 }
