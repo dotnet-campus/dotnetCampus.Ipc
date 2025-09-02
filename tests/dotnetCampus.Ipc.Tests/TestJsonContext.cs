2bf093daa5d76edc6c6eb3ef45310123570cebcf
walterlv <xblero@gmail.com>
2025-09-02 23:54:32 +0800

    修复 JSON 序列化在测试项目中的类型

--- tests/dotnetCampus.Ipc.Tests/TestJsonContext.cs
+++ tests/dotnetCampus.Ipc.Tests/TestJsonContext.cs
@@ -1,9 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+using dotnetCampus.Ipc.Tests.CompilerServices;
 
 namespace dotnetCampus.Ipc.Tests;
 
 #if NET6_0_OR_GREATER
 
-[JsonSerializable(typeof(bool))]
+[JsonSerializable(typeof(BindingFlags))]
+[JsonSerializable(typeof(List<string>))]
+[JsonSerializable(typeof(string[]))]
+[JsonSerializable(typeof(FakeIpcObjectSubModelA))]
+[JsonSerializable(typeof(IFakeIpcObject.NestedEnum))]
+[JsonSerializable(typeof((double a, uint b, int? c, byte d)))]
 internal partial class TestJsonContext : JsonSerializerContext;
 #endif
