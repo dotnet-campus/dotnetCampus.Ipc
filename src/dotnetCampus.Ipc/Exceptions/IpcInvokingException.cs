b21fc18d10d4cd2d1aacb8835972426fdf2c78cd
walterlv <xblero@gmail.com>
2025-09-04 15:30:18 +0800

    整理 IPC 对象异常

---
+++ src/dotnetCampus.Ipc/Exceptions/IpcInvokingException.cs
@@ -0,0 +1,8 @@
+﻿using dotnetCampus.Ipc.CompilerServices.GeneratedProxies.Models;
+
+namespace dotnetCampus.Ipc.Exceptions;
+
+/// <summary>
+/// 当获取设置属性值或调用方法时，如果实现方法内抛出了异常，且不是常见的异常类型（参见 <see cref="GeneratedProxyExceptionModel"/>），则会用此异常包装。
+/// </summary>
+internal class IpcInvokingException(string message, string? remoteStackTrace) : IpcRemoteException(message, remoteStackTrace);
