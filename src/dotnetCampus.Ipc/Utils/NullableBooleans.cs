7bbef255cefe29e0e17e2afb72fabe8282b3b93f
walterlv <xblero@gmail.com>
2025-09-08 20:06:04 +0800

    修复 NullableBoolean 的逻辑

--- src/dotnetCampus.Ipc/Utils/NullableBooleans.cs
+++ src/dotnetCampus.Ipc/Utils/NullableBooleans.cs
@@ -3,19 +3,19 @@
 namespace dotnetCampus.Ipc.Utils;
 
 /// <summary>
-/// 可存储 16 个可空布尔值的结构。使用 2 位来存储一个可空布尔值。
+/// 可存储 8 个可空布尔值和 16 个布尔值的结构。使用 2 位来存储一个可空布尔值。共 32 位。
 /// </summary>
 internal struct NullableBooleans
 {
     /// <summary>
-    /// 使用 2 位来存储一个可空布尔值，共 32 位可存储 16 个可空布尔值。
+    /// 高 16 位存储 16 个布尔值；低 16 位来存储 8 个可空布尔值。
     /// </summary>
     private uint _booleans;
 
     /// <summary>
-    /// 获取或设置指定索引处的可空布尔值，值范围 [0, 15]。
+    /// 获取或设置指定索引处的可空布尔值，值范围 [0, 7]。
     /// </summary>
-    /// <param name="index">索引，值范围 [0, 15]。</param>
+    /// <param name="index">索引，值范围 [0, 7]。</param>
     public bool? this[int index]
     {
         get => ((_booleans & (1 << (index * 2 + 1))) >> (index * 2 + 1), (_booleans & (1 << (index * 2))) >> (index * 2)) switch
@@ -39,13 +39,13 @@ internal struct NullableBooleans
     [Pure]
     public bool GetBooleanAt(int indexFromEnd)
     {
-        var index = 15 - indexFromEnd;
+        var index = 31 - indexFromEnd;
         return (_booleans & (1 << index)) != 0;
     }
 
     public void SetBooleanAt(int indexFromEnd, bool value)
     {
-        var index = 15 - indexFromEnd;
+        var index = 31 - indexFromEnd;
         _booleans = value
             ? _booleans | (uint)(1 << index)
             : _booleans & (uint)~(1 << index);
