0cd2a62538b3ecf4ce92921a82f6023ed84518ec
walterlv <xblero@gmail.com>
2025-11-12 16:15:40 +0800

    修复分析器异常

--- src/dotnetCampus.Ipc.Analyzers/Properties/Localizations.Designer.cs
+++ src/dotnetCampus.Ipc.Analyzers/Properties/Localizations.Designer.cs
@@ -39,7 +39,7 @@ namespace dotnetCampus.Ipc.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("dotnetCampus.Ipc.Properties.Resources", typeof(Localizations).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("dotnetCampus.Ipc.Properties.Localizations", typeof(Localizations).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
